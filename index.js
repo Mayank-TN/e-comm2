20a81d1b688b4d028df6b286126ccd641d5b9de3
Mayank-TN <mtandon10@gmail.com>
2022-12-27 23:47:56 +0530

    cart remove and total price fixes

--- index.js
+++ index.js
@@ -2,44 +2,45 @@ const cart = document.getElementById('cart');
 const cartItems = document.querySelector('.cart-items');
 const cartNumber = document.querySelector('.cart-number');
 const notification = document.getElementById('container');
+const cartTotal = document.getElementById('total-value');
 
 
 
 const dataArray = [
     {
-        name : 'album1' ,
-        price : '12.99' ,
-        img : './img/Album1.jpg'
-    } ,
-    {
-        name : 'album2' ,
-        price : '14.99' ,
-        img : './img/Album2.jpg'
+        name: 'album1',
+        price: 12.99,
+        img: './img/Album1.jpg'
     },
     {
-        name : 'album3' ,
-        price : '9.99' ,
-        img : './img/Album3.jpg'
+        name: 'album2',
+        price: 14.99,
+        img: './img/Album2.jpg'
     },
     {
-        name : 'album4' ,
-        price : '19.99' ,
-        img : './img/Album4.jpg'
+        name: 'album3',
+        price: 9.99,
+        img: './img/Album3.jpg'
     },
     {
-        name : 't-shirt' ,
-        price : '19.99' ,
-        img : './img/tshirt.jpg'
+        name: 'album4',
+        price: 19.99,
+        img: './img/Album4.jpg'
     },
     {
-        name : 'coffee-cup' ,
-        price : '6.99' ,
-        img : './img/coffee.jpg'
+        name: 't-shirt',
+        price: 19.99,
+        img: './img/tshirt.jpg'
+    },
+    {
+        name: 'coffee-cup',
+        price: 6.99,
+        img: './img/coffee.jpg'
     }
 ]
 
 
-const cartArray = [];
+let cartArray = [];
 
 
 
@@ -47,25 +48,28 @@ function addToCart(event) {
     const btn = String(event.target.id);
     if (btn.includes('btn')) {
         const [album, ele] = btn.split(' ');
-        const data = dataArray.find((data)=> data.name === album );
-        if(cartArray.includes(data)){
+        const data = dataArray.find((data) => data.name === album);
+        if (cartArray.includes(data)) {
             alert("Item already exist in cart")
         }
-        else{
+        else {
             cartArray.push(data);
             cartNumber.textContent = cartArray.length;
             notification.style.display = 'block';
             notification.innerHTML = `<span>Your Product : ${data.name} is added to cart</span>`
+            let total = 0;
+            cartArray.forEach((data) => total += data.price);
+            cartTotal.textContent = `${total}`;
         }
 
 
-        setTimeout(()=>{
+        setTimeout(() => {
             notification.style.display = 'none';
-        } , 3000)
-       
+        }, 3000)
+
     }
     cartItems.innerHTML = '';
-    cartArray.forEach((data)=>{
+    cartArray.forEach((data) => {
         const div = document.createElement('div');
         div.className = 'cart-row';
         div.innerHTML = `<span class='cart-item cart-column'>
@@ -78,15 +82,20 @@ function addToCart(event) {
         <button onclick={removeFromCart('${data.name}')}>REMOVE</button>
         </span>
         `;
-        cartItems.insertBefore(div , cartItems.children[0]);
+        cartItems.appendChild(div);
     })
 }
 
 
 
-function removeFromCart(data){
-    let array = cartArray.filter((item)=> item.name!==data)
-    console.log(array)
+function removeFromCart(data) {
+    let array = cartArray.filter((item) => item.name !== data)
+    cartArray = [...array];
+    cartNumber.textContent = cartArray.length;
+    let total = 0;
+    cartArray.forEach((data) => total += data.price);
+    cartTotal.textContent = `${total}`;
+
 }
 
 function hideCart() {
